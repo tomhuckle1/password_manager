9e7ace32e400ad5bf28688c2ae3dfa7a8471a429
Thomas Huckle <thomas.huckle@stcs.co.uk>
2026-02-28 20:30:40 +0000

    Fix category delete button not working

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,7 +1,9 @@
 import { initClickHandlers } from "./handlers/clickHandlers.js";
 import { initPasswordModal } from "./ui/password/modal.js";
+import { initCategoryDelete } from "./ui/category/delete.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initPasswordModal();
   initClickHandlers();
+  initCategoryDelete();
 });
