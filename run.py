b839c035be5b5c897f8a25fcc03bdc3d3378cdc1
Thomas Huckle <thomas.huckle@stcs.co.uk>
2026-03-01 19:24:12 +0000

    Only seed if db is empty

--- run.py
+++ run.py
@@ -4,7 +4,9 @@ from seed import main as seed_main
 app = create_app()
 
 with app.app_context():
-    seed_main()        
+    # Only seed if db is empty
+    if not User.query.first():
+        seed_main()        
 
 if __name__ == "__main__":
     app.run()
