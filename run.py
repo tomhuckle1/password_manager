c60a70aa6a6fcc9034e855a3c94c9ba599a7e8d7
Thomas Huckle <thomas.huckle@stcs.co.uk>
2026-03-01 14:42:00 +0000

    Update run.py for production on Render

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
-from app import create_app
+from app import create_app, db
+from seed import main as seed_main
 
 app = create_app()
+
+with app.app_context():
+    db.create_all()      # creates tables if missing
+    seed_main()        
 
 if __name__ == "__main__":
     app.run()
