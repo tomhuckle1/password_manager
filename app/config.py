ff40f187487e092ecd9a01eefd69f3f010ee4368
Thomas Huckle <thomas.huckle@stcs.co.uk>
2026-03-01 13:22:22 +0000

    Update config file to be production ready

--- app/config.py
+++ app/config.py
@@ -11,5 +11,12 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     ENCRYPTION_KEY = os.getenv("ENCRYPTION_KEY")
 
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", f"sqlite:///{DB_PATH}")
+    # Handle Postgres URL
+    database_url = os.getenv("DATABASE_URL")
+    if database_url and database_url.startswith("postgres://"):
+        # Replace postgres with postgresql to work with SQLAlchemy
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+    # Default to SQLite in the local environment if databse url is not set
+    SQLALCHEMY_DATABASE_URI = database_url or f"sqlite:///{DB_PATH}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
