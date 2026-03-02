cceb8c014363b2931ec114433eb78460ccc0969b
Thomas Huckle <thomas.huckle@stcs.co.uk>
2026-03-02 15:54:50 +0000

    Update user model

--- app/models/user.py
+++ app/models/user.py
@@ -17,7 +17,7 @@ class User(db.Model, UserMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False, index=True)
+    email = db.Column(db.String(100), unique=True, nullable=False, index=True)
     password_hash = db.Column(db.String(255), nullable=False)
     role = db.Column(db.String(20), nullable=False, default=ROLE_REGULAR)
 
